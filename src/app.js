740a342898990fe77a796b72c6a9d274de63342c
Angela Vawser <amvawser@gmail.com>
2019-07-31 21:52:04 -0700

    generated 3 random images, saving and removing from set item list - images render to browser

--- src/app.js
+++ src/app.js
@@ -1,2 +1,43 @@
-import store from './store.js';
+import store from './data/store.js';
+import ItemSet from './item-set.js';
+// import { getRandomInt, findById } from './util.js';
+// import displayItem from './display-item.js';
 
+
+const imgButtons = document.querySelectorAll('button[name=item]');
+const choiceContainer = document.getElementById('item-choice-container');
+const resultsContainer = document.getElementById('results-container');
+const resultsButton = document.getElementById('results-button');
+
+const items = store.getItems();
+const masterItemSet = new ItemSet(items);
+const selectedItems = [];
+const displayedItems = [];
+let itemsToDisplay = [];
+let userItem;
+let turns = 0;
+
+function renderItems() {
+    let itemSet = masterItemSet;
+    if(userItem && masterItemSet.list.length > 3) {
+        itemSet = new ItemSet(masterItemSet.list);
+        itemSet.removeById(userItem.id);
+    }
+
+    // generates 3 random imgs to display, stores and removes from item set
+    for(let i = 0; i < 3; i++) {
+        let renderItem = itemSet.getRandomItem();
+        itemsToDisplay.push(renderItem);
+        itemSet.removeById(renderItem.id);
+        let img = imgButtons[i].querySelector('img');
+        img.src = renderItem.image;
+        img.classList.remove('hidden');
+    }
+}
+renderItems();
+
+
+// for(let i = 0; i < radioInputs.length; i++) {
+//     const radioInput = radioInputs[i];
+//     radioInput.addEventListener('input', );
+// }
--- src/item-set.js
+++ src/item-set.js
@@ -7,16 +7,17 @@ class ItemSet {
     }
 
     getRandomItem() {
-        const index = getRandomInt(this.list.legnth);
+        const index = getRandomInt(this.list.length);
         const item = this.list[index];
         return item;
     }
+    
     removeById(itemId) {
         const list = this.list;
         for(let i = 0; i < list.length; i++) {
             const item = list[i];
             if(item.id === itemId); {
-                list.slice(i, 1);
+                list.splice(i, 1);
             }
             return;
         }
@@ -24,4 +25,3 @@ class ItemSet {
 }
 
 export default ItemSet;
-
