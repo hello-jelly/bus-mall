3fc4a4bf81a2a5b1d136ec1059d047abacd13772
Angela Vawser <amvawser@gmail.com>
2019-07-31 22:36:48 -0700

    test to remove item is passing

--- test/item-set.test.js
+++ test/item-set.test.js
@@ -16,3 +16,12 @@ test('Copying original item list', assert => {
     assert.deepEqual(itemSet.list, items);
     assert.notEqual(itemSet.list, items);
 });
+
+test('Should remove item from list by code', assert => {
+    const itemSet = new ItemSet(items);
+    itemSet.removeByCode('shark');
+    const sharkItem = itemSet.list.find((item) => {
+        return item.code === 'shark';
+    });
+    assert.equal(sharkItem, undefined);
+});
