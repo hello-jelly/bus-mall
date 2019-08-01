ee0fe2a58fe234c41fc25fa836cd42e5a29d2349
Angela Vawser <amvawser@gmail.com>
2019-08-01 11:55:18 -0700

    working on tallying up selected items

--- src/app.js
+++ src/app.js
@@ -3,16 +3,16 @@ import ItemSet from './item-set.js';
 // import { getRandomInt, findById } from './util.js';
 // import displayItem from './display-item.js';
 
-
+const items = store.getItems();
 const imgButtons = document.querySelectorAll('button[name=item]');
+const header = document.getElementById('header');
 const choiceContainer = document.getElementById('item-choice-container');
 const resultsContainer = document.getElementById('results-container');
 const resultsButton = document.getElementById('results-button');
 
-const items = store.getItems();
 let masterItemSet = new ItemSet(items);
-const selectedItems = [];
 const displayedItems = {};
+// let selectedItems = {};
 let itemsToDisplay;
 
 let turns = 0;
@@ -26,45 +26,52 @@ function tally(items, code) {
 }
 
 function renderItems() {
-    // Check for turn count here before continuing
-    if(turns > 5) {
+    // Checks for turn count, if 25 then disables and hides img buttons and reveals the results button
+    // when reveal button clicked it hides self and reveals chart
+    if(turns > 25) {
         imgButtons.disable;
         choiceContainer.classList.add('hidden');
+        header.classList.add('hidden');
         resultsButton.classList.remove('hidden');
+        
         resultsButton.addEventListener('click', (event) => { 
             resultsContainer.classList.remove('hidden');
-        })
-    } else {
-        if(masterItemSet.list.length < 3) {
-            masterItemSet = new ItemSet(items);
-            for(let i = 0; i < itemsToDisplay.length; i++) {
-                masterItemSet.removeByCode(itemsToDisplay[i].code);
-            }
-        }}
+            resultsButton.Button.classList.add('hidden');
+            return;
+        });
+    } else if(masterItemSet.list.length < 3) {
+        masterItemSet = new ItemSet(items);
+        for(let i = 0; i < itemsToDisplay.length; i++) {
+            masterItemSet.removeByCode(itemsToDisplay[i].code);
+        }
+    }
+}
 
-    let itemSet = masterItemSet;
+let itemSet = masterItemSet;
 
-    itemsToDisplay = [];
+itemsToDisplay = [];
     // generates 3 random imgs to display, stores and removes from item set
-    for(let i = 0; i < 3; i++) {
-        let renderItem = itemSet.getRandomItem();
-        itemsToDisplay.push(renderItem);
-        tally(displayedItems, renderItem.code);
-        itemSet.removeByCode(renderItem.code);
+for(let i = 0; i < 3; i++) {
+    let renderItem = itemSet.getRandomItem();
+    itemsToDisplay.push(renderItem);
+    tally(displayedItems, renderItem.code);
+    itemSet.removeByCode(renderItem.code);
 
         // displaying image and removing hidden class
-        let img = imgButtons[i].querySelector('img');
-        img.src = renderItem.image;
-        img.alt = renderItem.code;
-        img.classList.remove('hidden');
-    }
+    let img = imgButtons[i].querySelector('img');
+    img.src = renderItem.image;
+    img.alt = renderItem.code;
+    img.classList.remove('hidden');
 }
 
 choiceContainer.addEventListener('click', (event) => {
     event.preventDefault();
     turns++;
     renderItems();
-    console.log(turns);
 });
 
 renderItems();
+
+// for(let i = 0; i < 3; i++) {
+//     tally(selectedItems, selectedItems.code);
+// }
